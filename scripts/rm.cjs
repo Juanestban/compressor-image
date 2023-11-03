d8b28b95b995fa5b42f5179a1512bbaae225c5f9
Juan Esteban <juanestban000@gmail.com>
2023-11-03 11:58:50 -0500

    fix: error to build and try copy folder dist to tests folder for test the new features

--- scripts/rm.cjs
+++ scripts/rm.cjs
@@ -1,24 +1,41 @@
 const child_process = require('node:child_process');
+const fs = require('node:fs/promises');
 const path = require('node:path');
 
 console.log('🤖 doing prebuild');
 console.time('time building');
 
 const isWindows = process.platform === 'win32';
-const cmdRemove = isWindows ? 'powershell.exe Remove-Item' : 'sh rm';
-const cmdCopy = isWindows ? 'powershell.exe cp' : 'sh cp';
 const withFlags = isWindows ? '' : '-rf';
 
 const getPath = (...args) => path.resolve(process.cwd(), ...args);
 
-try {
-  child_process.spawnSync(`${cmdRemove} ${withFlags} ${getPath('dist')}`, { shell: true });
-  child_process.spawnSync(`${cmdRemove} ${withFlags} ${getPath('tests/dist')}`, { shell: true });
-  child_process.execSync(`pnpm tsc`);
-  child_process.spawnSync(`${cmdCopy} -Recurse ${getPath('dist/*')} ${getPath('tests/dist')}`);
-} catch (error) {
-  console.error('🔴 error to exec `tsc` command\n', error.message);
-}
+const distPath = getPath('dist');
+const distTestPath = getPath('tests/dist');
 
-console.timeEnd('time building');
-console.log('💚 done ts-build');
+(async () => {
+  try {
+    console.log(`[🔹] cleaning dist folder: [${distPath}]`);
+    await fs.unlink(distPath).catch(() => {});
+    await fs.rm(distPath, { recursive: true, force: true }).catch(() => {});
+    console.log('[💚] done clean dist folder');
+
+    console.log('[🔹] cleaning', 'path: `tests/dist`', ` folder: [${distPath}]`);
+    await fs.unlink(distTestPath).catch(() => {});
+    await fs.rm(distTestPath, { recursive: true, force: true }).catch(() => {});
+    console.log('[💚] done clean dist folder');
+
+    console.log(`[🔹] building...`);
+    child_process.execSync('pnpm tsc');
+    console.log('[💚] done clean dist folder');
+
+    console.log('[🔹] copy dist folder to `tests` folder');
+    await fs.cp(distPath, distTestPath, { recursive: true });
+    console.log('[💚] done copy dist folders');
+  } catch (error) {
+    console.error('[🔴] error to exec `tsc` command\n', error.message);
+  }
+
+  console.timeEnd('time building');
+  console.log('💚 done ts-build');
+})();
